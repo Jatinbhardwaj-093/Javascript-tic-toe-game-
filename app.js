d51a596408ff8cecebe0c1f73da5849bb8097e34
Jatinbhardwaj-093 <bhardwajjatin093@gmail.com>
2024-09-18 20:59:00 +0530

    Upgrade the look and logic block of winning block

--- app.js
+++ app.js
@@ -10,43 +10,59 @@ const app = Vue.createApp({
     },
 
     methods: {
+        removingWinnerBlock() {
+            for (let i = 0; i < 9; i++) {
+                let p = "box" + String(i);
+                this.$refs[p].classList.remove("winnerblock");
+            }
+        },
         reset() {
             this.boxes = ['', '', '', '', '', '', '', '', '']
             this.player = 'X'
             this.winner = null
             this.positions = null
+            this.removingWinnerBlock()
         } ,
         checkWinner() {
             if (this.boxes[0] === this.boxes[1] && this.boxes[1] === this.boxes[2] && this.boxes[0] !== '') {
                 this.winner = this.boxes[0]
-                return this.positions=[0,1,2]
+                this.positions=[0,1,2]
             } else if (this.boxes[3] === this.boxes[4] && this.boxes[4] === this.boxes[5] && this.boxes[3] !== '') {
                 this.winner = this.boxes[3]
-                return this.positions=[3,4,5]
+                this.positions=[3,4,5]
             } else if (this.boxes[6] === this.boxes[7] && this.boxes[7] === this.boxes[8] && this.boxes[6] !== '') {
                 this.winner = this.boxes[6] 
-                return this.positions=[6,7,8]
+                this.positions=[6,7,8]
             } else if (this.boxes[0] === this.boxes[3] && this.boxes[3] === this.boxes[6] && this.boxes[0] !== '') {
                 this.winner = this.boxes[0]
-                return this.positions=[0,3,6]
+                this.positions=[0,3,6]
             } else if (this.boxes[1] === this.boxes[4] && this.boxes[4] === this.boxes[7] && this.boxes[1] !== '') {
                 this.winner = this.boxes[1]
-                return this.positions=[1,4,7]
+                this.positions=[1,4,7]
             } else if (this.boxes[2] === this.boxes[5] && this.boxes[5] === this.boxes[8] && this.boxes[2] !== '') {
                 this.winner = this.boxes[2]
-                return this.positions=[2,5,8]
+                this.positions=[2,5,8]
             } else if (this.boxes[0] === this.boxes[4] && this.boxes[4] === this.boxes[8] && this.boxes[0] !== '') {
                 this.winner = this.boxes[0]
-                return this.positions=[0,4,8]
+                this.positions=[0,4,8]
             } else if (this.boxes[2] === this.boxes[4] && this.boxes[4] === this.boxes[6] && this.boxes[2] !== '') {
                 this.winner = this.boxes[2] 
-                return this.positions=[2,4,6]
+                this.positions=[2,4,6]
             }
         },
         
         draw() {
             if (this.boxes.indexOf('') === -1 && this.winner == null) {
                 this.winner='Draw'
+            }
+        },
+
+        winnerblock() {
+            if (this.positions != null) {
+                for (let i = 0; i < this.positions.length; i++) {
+                    let p = "box" + String(this.positions[i]);
+                    this.$refs[p].classList.add("winnerblock");
+                }
             }
         },
         
@@ -60,6 +76,7 @@ const app = Vue.createApp({
             this.checkWinner() 
             this.draw()
             this.disabled()
+            this.winnerblock()
             
         },
 
